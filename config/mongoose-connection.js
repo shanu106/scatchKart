d65bd656c9fcec72f437fa4bf2bd7131eceb8fa5
Shahnawaj Rangrej <SHAHNAWAJNILGER244@GMAIL.COM>
2024-11-16 19:39:27 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
-const dbgr = require("debug")("development:mongoose");
-const config = require('config');
-mongoose
-.connect(`${config.get("mongodb+srv://Shahnawaj_Rangrej:Kohinoor@0595@scatchkart.tkur8.mongodb.net/?retryWrites=true&w=majority&appName=ScatchKart")}/ScatchProject`)
-.then(function(){
-    dbgr("connect");
-    
-})
-.catch(function(err){
-    dbgr(err);
-    
-})
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error: ', err));
+
 
 module.exports= mongoose.connection;
