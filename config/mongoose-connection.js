30029caf8354f86cc89d1d3464b6ee509cce4541
Shahnawaj Rangrej <SHAHNAWAJNILGER244@GMAIL.COM>
2024-11-16 19:35:57 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dbgr = require("debug")("development:mongoose");
 const config = require('config');
 mongoose
-.connect(`${config.get("mongodb+srv://Shahnawaj_Rangrej:<Kohinoor@0595>@scatchkart.tkur8.mongodb.net/?retryWrites=true&w=majority&appName=ScatchKart")}/ScatchProject`)
+.connect(`${config.get("mongodb+srv://Shahnawaj_Rangrej:Kohinoor@0595@scatchkart.tkur8.mongodb.net/?retryWrites=true&w=majority&appName=ScatchKart")}/ScatchProject`)
 .then(function(){
     dbgr("connect");
     
